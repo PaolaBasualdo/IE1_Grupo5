9f7921b5391e5d87f1a3ee56f3c437c6d8e0f064
GiseCristaldo <giselle.cristaldo.1994@gmail.com>
2025-05-25 23:13:07 -0300

    Completado el archivo raiz index.js con las importaciones de modelos y rutas. Solo falta inicializar la DB

--- index.js
+++ index.js
@@ -6,10 +6,32 @@
     // IMPORTAR LOS MODELOS para que Sequelize los registre
   import Departamento from "./src/models/Departamento.js";
   import Instructor from './src/models/Instructor.js';
+  import Aula from './src/models/Aula.js';
+  import Carrera from './src/models/Carrera.js';
+  import CarreraxEstudiante from './src/models/CarreraxEstudiante.js';
+  import ComisionFunction from "./src/models/Comision.js"; 
+  import Curso from "./src/models/Curso.js"
+  import Estudiante from './src/models/Estudiante.js';
+  import Inscripcion from './src/models/Inscripcion.js';
+  import Prerrequisito from './src/models/Prerrequisito.js';
 
     // IMPORTAR LAS RUTAS
     import departamentoRoutes from './src/routes/departamento.routes.js';
     import instructorRoutes from './src/routes/instructor.routes.js';
+    import aulaRoutes from './src/routes/aula.routes.js';
+    import carreraRoutes from './src/routes/carrera.routes.js';
+    import carreraxEstudianteRoutes from './src/routes/carreraxEstudiante.routes.js';
+    import comisionRoutes from './src/routes/comision.routes.js';
+    import cursoRoutes from './src/routes/curso.routes.js';
+    import estudianteRoutes from './src/routes/estudiante.routes.js';
+    import inscripcionRoutes from './src/routes/inscripcion.routes.js';
+    import prerrequisitoRoutes from './src/routes/prerrequisito.routes.js'
+
+
+    //inicializo los que son funciones
+    const CarreraModelo = Carrera(sequelize);
+    const ComisionModelo = ComisionFunction(sequelize);
+
      
    const app = express();  
      
@@ -21,6 +43,18 @@
 // usar las rutas
   app.use('/api/departamentos', departamentoRoutes);
   app.use('/api/instructores', instructorRoutes);
+  app.use('/api/aulas', aulaRoutes);
+  app.use('/api/carreras', carreraRoutes);
+  app.use('/api/carreraxEstudiantes', carreraxEstudianteRoutes);
+  app.use('/api/comisiones', comisionRoutes);
+  app.use('/api/cursos', cursoRoutes);
+  app.use('/api/estudiantes', estudianteRoutes);
+  app.use('/api/inscripciones', inscripcionRoutes);
+  app.use('/api/prerrequisitos', prerrequisitoRoutes);
+  
+
+
+  
 
      
    // Ruta de prueba  
